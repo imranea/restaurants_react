9173ef780064231fc5e57e7207e068e6050c96af
Imrane Abdoul <imraneabdoul@gmail.com>
2020-11-29 00:06:05 +0100

    delete file noteRestaurant in store and implement the function dispatch in action.js

--- src/store/action.js
+++ src/store/action.js
@@ -2,10 +2,11 @@ export const NOTE= "NOTE"
 export const FETCH_RESTAURANT = "FETCH_RESTAURANT"
 export const FETCH_RESTAURANT_REVIEW = "FETCH_RESTAURANT_REVIEW"
 
-export const noteRestaurant = (note) =>({ // function to update rating
-    type : NOTE,
-    note
-})
+export const noteRestaurant = (note) =>{ // function to update rating
+    return dispatch =>{
+        dispatch({type:NOTE,note})
+    }
+}
 
 export const fetchRestaurant = (restaurants) =>({ // function to fill our state restaurants
     type : FETCH_RESTAURANT,
--- src/store/noteRestaurant.js
+++
@@ -1,7 +0,0 @@
-import {noteRestaurant} from "./action"
-
-export default (note)=>{
-        return dispatch=>{
-            dispatch(noteRestaurant(note))
-    }
-}
